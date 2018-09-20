4d36224b247aeb00edc0e38f7390267b9592105c
Petar Kostic <snookik@gmail.com>
2018-09-20 16:43:15 +0200

    Finished Ex. 3 the proper way

--- Petar Kostic/Lab3.hs
+++ Petar Kostic/Lab3.hs
@@ -61,14 +61,16 @@ testParser = do let input = map show forms
 -- Note that forms can be of any form of input, the function assesses if the parse result was done correctly in relation to
 -- the input by the use of `elem`
 
--- | Exercise 3 - Time: 
--- Step 1: use the equivalence between p→q and ¬p∨q to get rid of → symbols
--- Step 2: use the equivalence of p↔q and (¬p∨q)∧(p∨¬q) to get rid of ↔ symbols
--- Step 3: p↔q and (p∧q)∨(¬p∧¬q) is also equivalent
--- Step 4: check for conjunction of disjunction of literals (p v q) ^ (-p v q)
+-- | Exercise 3 - Time: 3 hours
+-- Pipeline explanation
+-- Step 1: use the equivalence between p→q and ¬p∨q to get rid of → symbols       |(arrowFree)
+-- Step 2: use the equivalence of p↔q and (¬p∨q)∧(p∨¬q) to get rid of ↔ symbols   |(nnf)
+-- Step 3: p↔q and (p∧q)∨(¬p∧¬q) is also equivalent                               |(nnf)
+-- Step 4: Distribute conjunction over disjunction (p^q)vr = (p^r)^(qvr)          |(cnf' -> dist)
+-- Step 5: Flatten out the conjunctions, a.k.a. associativity law                 |(flat)
 
 cnf :: Form -> Form
-cnf = flatdsj . flat . cnf' . nnf . arrowfree
+cnf = flat . cnf' . nnf . arrowfree
     where cnf' :: Form -> Form
           cnf' (Cnj [f1,f2])       = Cnj [cnf' f1, cnf' f2]
           cnf' (Dsj [f1,f2])       = dist (head [cnf' f1, cnf' f2]) ([cnf' f1, cnf' f2] !! 1) 
@@ -79,23 +81,38 @@ cnf = flatdsj . flat . cnf' . nnf . arrowfree
           dist e1 e2               = Dsj [e1,e2]
 
 flat :: Form -> Form
-flat (Dsj [Dsj f1, Dsj f2])       = Dsj ((map flat f1) ++ (map flat f2))
-flat (Dsj [Dsj f1, Prop x])       = Dsj ((map flat f1) ++ [Prop x])
-flat (Dsj [Dsj f1, Neg (Prop x)]) = Dsj ((map flat f1) ++ [Neg (Prop x)])
-flat (Cnj f)                      = Cnj (map flat f)
+flat (Cnj fs)        = Cnj $ foldr (f . flat) [] fs 
+                     where f (Cnj xs) ys = xs ++ ys
+                           f expr ys     = expr : ys
+flat (Dsj fs)        = Dsj $ foldr (f . flat) [] fs
+                     where f (Dsj xs) ys = xs ++ ys
+                           f expr ys     = expr : ys
+flat (Neg f)         = Neg (flat f)
+-- flat (f1 `Impl` f2)  = flat f1 `Impl` flat f2
+-- flat (f1 `Equiv` f2) = flat f1 `Equiv` flat f2
 flat expr = expr
 
-flatdsj :: Form -> Form
-flatdsj (Cnj [Cnj f1, Cnj f2])   = Cnj ((map flatdsj f1) ++ (map flatdsj f2))
-flatdsj (Cnj [Cnj f1, Neg (Prop x)])   = Cnj ((map flatdsj f1) ++ [Neg (Prop x)])
-flatdsj (Cnj [Cnj f1, Prop x])         = Cnj ((map flatdsj f1) ++ [Prop x])
---flatdsj (Cnj f1)                       = Cnj (map flatdsj f1)
-flatdsj (Cnj list)                     = Cnj (map flatdsj [f | f <- list])
-flatdsj expr                           = expr
-    
--- (((-a v b a) ^ (-a v b -b)) ^ ((-a v b -b) ^ (-a v b a)) ((b v -a a) ^ (b v -a -b))((b v -a -b) ^ (b v -a a)))
--- ((((-a v b a) ^ (-a v b -b)) ^ ((-a v b -b) ^ (-a v b a))) ^ (((b v -a a) ^ (b v -a-b)) ^ ((b v -a -b) ^ (b v -a a))))
+-- 
+-- Before Flatten
+-- (((((-a v b) v a) ^ ((-a v b) v -b)) ^ (((-a v b) v -b) ^ ((-a v b) v a))) ^ ((((b v -a) v a) ^ ((b v -a) v -b)) ^ (((b v -a) v -b) ^ ((b v -a) v a))))"
+-- After flatten
+-- ((-a v b a) ^ (-a v b v -b) ^ (-a v b v -b) ^ (-a v b v a) ^ (b v -a v a) ^ (b v -a v -b) ^ (b v -a -b) ^ (b v -a v a))"
 
-justHelper :: Maybe [a] -> [a]
-justHelper (Just x) = x
-justHelper Nothing = []
+-- The next inteteresting thing, although not required, 
+
+-- From session with Ana
+-- flat :: Form -> Form
+-- flat (Dsj [Dsj f1, Dsj f2])       = Dsj ((map flat f1) ++ (map flat f2))
+-- flat (Dsj [Dsj f1, Prop x])       = Dsj ((map flat f1) ++ [Prop x])
+-- flat (Dsj [Dsj f1, Neg (Prop x)]) = Dsj ((map flat f1) ++ [Neg (Prop x)])
+-- flat (Cnj f)                      = Cnj (map flat f)
+-- flat expr = expr
+
+-- flatdsj :: Form -> Form
+-- flatdsj (Cnj [Cnj f1, Cnj f2])   = Cnj ((map flatdsj f1) ++ (map flatdsj f2))
+-- flatdsj (Cnj [Cnj f1, Neg (Prop x)])   = Cnj ((map flatdsj f1) ++ [Neg (Prop x)])
+-- flatdsj (Cnj [Cnj f1, Prop x])         = Cnj ((map flatdsj f1) ++ [Prop x])
+-- --flatdsj (Cnj f1)                       = Cnj (map flatdsj f1)
+-- flatdsj (Cnj list)                     = Cnj (map flatdsj [f | f <- list])
+-- flatdsj expr                           = expr
+
