8095468c43538838482dcaa79137ce971d9f4316
Petar Kostic <snookik@gmail.com>
2018-09-25 23:23:14 +0200

    Added bonus to Group and finished personal Ex4

--- Petar Kostic/Lab3.hs
+++ Petar Kostic/Lab3.hs
@@ -2,6 +2,7 @@
 module Lab3 where
  
 import Data.List
+import Data.Char
 import System.Random
 import Test.QuickCheck
 import Data.Maybe
@@ -34,40 +35,28 @@ equivTestForm2 = Cnj [q,p]
 
 -- The implementation is fairly simple, and the test are pretty rudementary. However, due to the 
 
--- lexer :: String -> [Token]
--- lexer [] = []
--- lexer (c:cs) | isSpace c = lexer cs            -- ignore spaces
---              | isDigit c = lexNum (c:cs)       -- literal
--- lexer ('(':cs) = TokenOP : lexer cs            -- open parenthesis  (
--- lexer (')':cs) = TokenCP : lexer cs            -- close parenthesis )
--- lexer ('*':cs) = TokenCnj : lexer cs           -- Conjunction ^ = *
--- lexer ('+':cs) = TokenDsj : lexer cs           -- Disjuncton  v = +
--- lexer ('-':cs) = TokenNeg : lexer cs           -- Negation - 
--- lexer ('=':'=':'>':cs) = TokenImpl : lexer cs  -- implication ==>
--- lexer ('<':'=':'>':cs) = TokenEquiv : lexer cs -- Eqivalence <=>
--- lexer (x:_) = error ("unknown token: " ++ [x]) 
-
--- The show instance converts the datatype Form to the correct representation for the lexer
-
--- | Exercise 2 - Time: 30 min
+-- | Exercise 2 - Time: 25min
 forms :: [Form]
 forms = [form1, form2, form3]
 
 testParser :: IO Bool
-testParser = do let input = map show forms
-                let result = concatMap parse input
-                return $ all (`elem` forms) result
+testParser = do let input = map show forms          -- Convert list of input to strings list of strings
+                let result = concatMap parse input  -- Parse the input
+                return (all (`elem` forms) result)  -- Compare to the initial list, if they're the same,
+                                                    -- return true else false
 
--- Note that forms can be of any form of input, the function assesses if the parse result was done correctly in relation to
--- the input by the use of `elem`
+-- Form generation (Exercise 4) can be used to get more input for testing purposes
 
--- | Exercise 3 - Time: 3 hours
+-- | Exercise 3 - Time: 3,25 hours
 -- Pipeline explanation
 -- Step 1: use the equivalence between p→q and ¬p∨q to get rid of → symbols       |(arrowFree)
--- Step 2: use the equivalence of p↔q and (¬p∨q)∧(p∨¬q) to get rid of ↔ symbols   |(nnf)
--- Step 3: p↔q and (p∧q)∨(¬p∧¬q) is also equivalent                               |(nnf)
--- Step 4: Distribute conjunction over disjunction (p^q)vr = (p^r)^(qvr)          |(cnf' -> dist)
--- Step 5: Flatten out the conjunctions, a.k.a. associativity law                 |(flat)
+-- Step 2: use the equivalence of p↔q and (¬p∨q)∧(p∨¬q) to get rid of ↔ symbols   |(arrowFree)
+-- Step 3: p↔q and (p∧q)∨(¬p∧¬q) is also equivalent                                |(arrowFree)
+-- Step 4: conversion to negation normal form Use the equivalence between ¬¬ϕ and ϕ,| (nnf)
+-- Step 5: Use the equivalence between ¬(ϕ∧ψ) and ¬ϕ∨¬ψ,                            | (nnf)
+-- Step 6: Use the equivalence between ¬(ϕ∨ψ)and ¬ϕ∧¬ψ                              | (nnf)
+-- Step 7: Distribute conjunction over disjunction (p^q) v r = (p^r)^(qvr)          |(cnf' -> dist)
+
 
 cnf :: Form -> Form
 cnf = flat . cnf' . nnf . arrowfree
@@ -80,6 +69,13 @@ cnf = flat . cnf' . nnf . arrowfree
           dist e1 (Cnj [e21,e22])  = Cnj [e1 `dist` e21, e1 `dist` e22]
           dist e1 e2               = Dsj [e1,e2]
 
+-- Step 6: associative propery: p ∧ q ∧ r ≡ (p ∧ q) ∧ r ≡ p ∧ ( q ∧ r)              | (flat)
+-- For the list of conjunctions starting at the right form, iterate (foldr) with the empty list base
+-- over the list of different subforms in the conjunction. If there is a case where one conjunction appears in this list,
+-- the associative property above specifies that it can be concatenated here. If it is a different expression, nothing really
+-- happens as it gets put in gets pushed on the resulting list. However, the recursive call will check that, and the rest, again.
+-- Inspiration from reduction section here: https://stackoverflow.com/questions/16701186/recursion-in-treefold-function 
+
 flat :: Form -> Form
 flat (Cnj fs)        = Cnj $ foldr (f . flat) [] fs 
                      where f (Cnj xs) ys = xs ++ ys
@@ -88,31 +84,79 @@ flat (Dsj fs)        = Dsj $ foldr (f . flat) [] fs
                      where f (Dsj xs) ys = xs ++ ys
                            f expr ys     = expr : ys
 flat (Neg f)         = Neg (flat f)
--- flat (f1 `Impl` f2)  = flat f1 `Impl` flat f2
--- flat (f1 `Equiv` f2) = flat f1 `Equiv` flat f2
-flat expr = expr
+flat expr            = expr
 
--- 
--- Before Flatten
--- (((((-a v b) v a) ^ ((-a v b) v -b)) ^ (((-a v b) v -b) ^ ((-a v b) v a))) ^ ((((b v -a) v a) ^ ((b v -a) v -b)) ^ (((b v -a) v -b) ^ ((b v -a) v a))))"
+
+-- form 1 with different show
+-- ((a==>b)<=>(-b==>-a))
+-- After arrowfree
+-- (((-a v b) ^ (--b v -a)) v (-(-a v b) ^ -(--b v -a)))
+-- After nnf
+-- (((-a v b) ^ (b v -a)) v ((a ^ -b) ^ (-b ^ a)))
+-- After cnf'
+-- (((((-a v b) v a) ^ ((-a v b) v -b)) ^ (((-a v b) v -b) ^ ((-a v b) v a))) ^ ((((b v -a) v a) ^ ((b v -a) v -b)) ^ (((b v -a) v -b) ^ ((b v -a) v a))))
 -- After flatten
--- ((-a v b a) ^ (-a v b v -b) ^ (-a v b v -b) ^ (-a v b v a) ^ (b v -a v a) ^ (b v -a v -b) ^ (b v -a -b) ^ (b v -a v a))"
+-- ((-a v b v a) ^ (-a v b v -b) ^ (-a v b v -b) ^ (-a v b v a) ^ (b v -a v a) ^ (b v -a v -b) ^ (b v -a -b) ^ (b v -a v a))
 
--- The next inteteresting thing, although not required, 
+-- The next step would be to prune things like (-a v b v a), (tautology, always true). Although an attempt was made, I spent more time on Exercise 4 as that was more
+-- productive for the grading.
 
--- From session with Ana
--- flat :: Form -> Form
--- flat (Dsj [Dsj f1, Dsj f2])       = Dsj ((map flat f1) ++ (map flat f2))
--- flat (Dsj [Dsj f1, Prop x])       = Dsj ((map flat f1) ++ [Prop x])
--- flat (Dsj [Dsj f1, Neg (Prop x)]) = Dsj ((map flat f1) ++ [Neg (Prop x)])
--- flat (Cnj f)                      = Cnj (map flat f)
--- flat expr = expr
+-- | Exercisse 4 - Time: 1 hour
+-- Argument n determines 'levels' of the form. Random literals between 0 and 10 get replaced by 
+-- new logical operators (genSubForm)
+-- genForm :: Int -> IO Form 
+-- genForm n = do randomLength <- getRandomInt 10
+--                new <- genForm' n (map Prop [1 .. (randomLength + 1)])
+--                return (Cnj new)
 
--- flatdsj :: Form -> Form
--- flatdsj (Cnj [Cnj f1, Cnj f2])   = Cnj ((map flatdsj f1) ++ (map flatdsj f2))
--- flatdsj (Cnj [Cnj f1, Neg (Prop x)])   = Cnj ((map flatdsj f1) ++ [Neg (Prop x)])
--- flatdsj (Cnj [Cnj f1, Prop x])         = Cnj ((map flatdsj f1) ++ [Prop x])
--- --flatdsj (Cnj f1)                       = Cnj (map flatdsj f1)
--- flatdsj (Cnj list)                     = Cnj (map flatdsj [f | f <- list])
--- flatdsj expr                           = expr
+-- genForm' :: Int -> [Form] -> IO [Form]
+-- genForm' 0 xs = return xs 
+-- genForm' l xs = do newForms <- mapM genSubForm xs
+--                    genForm' (l-1) newForms
 
+-- genSubForm :: Form -> IO Form
+-- genSubForm (Prop x)   = do n <- getRandomInt 5
+--                            return (substituteLiterals n (Prop x))
+-- genSubForm (Impl a b) = do newFormA <- genSubForm a
+--                            newFormB <- genSubForm b
+--                            return (Impl newFormA newFormB)
+-- genSubForm (Dsj xs)   = do newForms <- mapM genSubForm xs
+--                            return (Dsj newForms)
+-- genSubForm (Cnj xs)   = do newForms <- mapM genSubForm xs
+--                            return (Cnj newForms)
+-- genSubForm (Neg x)    = do newForm <- genSubForm x
+--                            return (Neg newForm)
+-- genSubForm f          =    return f
+
+-- substituteLiterals :: Int -> Form -> Form
+-- substituteLiterals 0 (Prop n) = Dsj   [Prop n, Prop (n + 1)]
+-- substituteLiterals 1 (Prop n) = Cnj   [Prop n, Prop (n + 1)]
+-- substituteLiterals 2 (Prop n) = Impl  (Prop n) (Prop (n + 1))
+-- substituteLiterals 3 (Prop n) = Equiv (Prop n) (Prop (n + 1))
+-- substituteLiterals 4 (Prop n) = Neg   (Prop n)
+-- substituteLiterals 5 (Prop n) = Prop   n
+
+-- getRandomInt :: Int -> IO Int
+-- getRandomInt n = getStdRandom (randomR (0,n))
+
+-- -- Checks if a Form is cnf based on CNF's properties (conjuctions with one level of disjunctions.
+-- isCNF :: Form -> Bool
+-- isCNF (Cnj xs) = all isDis xs || all isLit xs
+--                   where isDis (Dsj xs)       = all isLit xs 
+--                         isDis _              = False
+--                         isLit (Prop n)       = True
+--                         isLit (Neg (Prop n)) = True
+--                         isLit _              = False
+-- isCNF _                                      = False
+
+-- -- The properties tested are isCNF and logical equivelance.
+-- -- logical equivelance is a weaker property than isCNF. Logical equivalence can be achieved by any logical permutation.
+-- testR :: Int -> Int -> IO ()
+-- testR k n = if k == n then print (show n ++ " tests passed")
+--                 else do
+--                   xs <- genForm 3
+--                   let cnfver = cnf xs
+--                   if isCNF cnfver && equiv xs cnfver then
+--                     do print ("pass on: " ++ show xs)
+--                        testR (k+1) n
+--                   else error ("failed test " ++ intToDigit(k) : " on: " ++ show xs)
